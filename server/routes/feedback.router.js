6ede2e916b1a07a8ea38d824c4d9e7b5c01dda8d
Billy Salinas <wsalinas315@gmail.com>
2022-12-04 00:33:05 -0600

    router POST written

--- server/routes/feedback.router.js
+++ server/routes/feedback.router.js
@@ -6,7 +6,18 @@ const pool = require('../modules/pool.js');
 
 
 // POST for review page
+router.get('/', (req, res) => {
+  let feedback = req.body;
+  console.log('Adding feedback entry', feedback);
 
+  let queryText = `INSERT INTO "feedback" ("feeling", "understanding", "support", "comments")
+                  VALUES ($1, $2, $3, $4);`;
+  pool.query(queryText, [feedback.feeling, feedback.understanding, feedback.support, feedback.comments]).then(result =>{
+    res.sendStatus(201);
+  }).catch(error => {
+    console.log(`Error adding new feedback`, error);
+  });
+});
 
 // DELETE for admin functionality
 

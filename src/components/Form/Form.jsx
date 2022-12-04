9c15f064960b5796f131a50f94f039bdc6837037
Billy Salinas <wsalinas315@gmail.com>
2022-12-03 23:10:33 -0600

    Completed Comments portion of the Form.jsx

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,9 +2,9 @@ import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
 import './Form.css';
-import { Button, FormControl, InputLabel, MenuItem, Select } from "@mui/material";
+import { Button, FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material";
 
-export default function Form({typeData, property, question, inputLabel, nextBtn, prevBtn}){
+export default function Form({ typeData, property, question, inputLabel, nextBtn, prevBtn }) {
 
   // history
   const history = useHistory();
@@ -20,46 +20,54 @@ export default function Form({typeData, property, question, inputLabel, nextBtn,
 
   // submit data to the store and move to the next form item
   const updateData = (event) => {
-    console.log('INPUT DATA:', inputData);
-    console.log('type:', typeData);
+    // console.log('NEXT BTN PATH:', nextBtn);
+    // console.log('type:', typeData);
+    // console.log('INPUT DATA:', inputData);
     event.preventDefault();
     dispatch({
       type: typeData,
       payload: inputData
     })
-    console.log('NEXT BTN PATH:', nextBtn);
     history.push(nextBtn);
   }
 
   // function to go back a page
-  const goBack = () =>{
+  const goBack = () => {
     history.push(prevBtn);
   }
 
   return (
-      <form className="form-body" onSubmit={(event) => updateData(event)}>
-        <h1>{question}</h1>
-        <br />
-        {inputLabel != "Feeling?" && <Button onClick={() => goBack()}>PREVIOUS</Button>}
-        <div className="inputs">
-          {inputLabel == 'Comments' ? <Comments /> :
-            <FormControl fullWidth>
-              <InputLabel>{inputLabel}</InputLabel>
-              <Select value={inputData} 
-                      label={inputLabel} 
-                      onChange={(event) => setInputData(event.target.value)}>
-                <MenuItem value={1}>1</MenuItem>
-                <MenuItem value={2}>2</MenuItem>
-                <MenuItem value={3}>3</MenuItem>
-                <MenuItem value={4}>4</MenuItem>
-                <MenuItem value={5}>5</MenuItem>
-              </Select>
-            </FormControl>
+    <form className="form-body" onSubmit={(event) => updateData(event)}>
+      <h1>{question}</h1>
+      <br />
+      <div className="inputs">
+        <div className="input-boxes">{inputLabel != "Feeling?" && <Button variant="contained" onClick={() => goBack()}>PREVIOUS</Button>}</div>
+        <div className="input-boxes">{inputLabel == 'Comments' ?
+          <FormControl fullWidth>
+            <TextField required 
+                       value={inputData} 
+                       label={inputLabel} 
+                       onChange={(event) => setInputData(event.target.value)} 
+                       variant="standard" />
+          </FormControl>
+          :
+          <FormControl fullWidth>
+            <InputLabel>{inputLabel}</InputLabel>
+            <Select required
+                    value={inputData}
+                    label={inputLabel}
+                    onChange={(event) => setInputData(event.target.value)}>
+              <MenuItem value={1}>1</MenuItem>
+              <MenuItem value={2}>2</MenuItem>
+              <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={4}>4</MenuItem>
+              <MenuItem value={5}>5</MenuItem>
+            </Select>
+          </FormControl>
           }
         </div>
-        <div>
-          <Button type="submit">NEXT</Button>
-        </div>
-      </form>
+        <div className="input-boxes"><Button variant="contained" type="submit">NEXT</Button></div>
+      </div>
+    </form>
   )
 }

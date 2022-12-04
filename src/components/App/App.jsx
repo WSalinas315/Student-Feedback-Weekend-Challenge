ef86254721171053f210fed280e650d965a610a6
Billy Salinas <wsalinas315@gmail.com>
2022-12-03 19:10:34 -0600

    Header moved to its own component. Router setup and ready to take in additional components.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,44 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
+import { HashRouter as Router, Route, Link } from 'react-router-dom';
+import Header from '../Header/Header';
+
 
 function App() {
 
   return (
     <div className='App'>
-      <header className='App-header'>
-        <h1 className='App-title'>Feedback!</h1>
-        <h4>Don't forget it!</h4>
-      </header>
+      <Router>
+        <Route exact path='/'>
+          <Header />
+          
+        </Route>
+        <Route exact path='/understanding'>
+          <Header />
+
+        </Route>
+        <Route exact path='/support'>
+          <Header />
+
+        </Route>
+        <Route exact path='/comments'>
+          <Header />
+
+        </Route>
+        <Route exact path='/review'>
+          <Header />
+
+        </Route>
+        <Route exact path='/submitted'>
+          <Header />
+
+        </Route>
+        <Route exact path='/admin'>
+          <Header />
+
+        </Route>
+      </Router>
     </div>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+
+export default function Header() {
+  <header className='App-header'>
+    <h1 className='App-title'>Feedback!</h1>
+    <h4>Don't forget it!</h4>
+  </header>
+}
